71fa482f7f4012a891d1adcd17c49791a06d4506
evgenyvodyannikov <vodyannikov.eugene@gmail.com>
2022-12-12 22:39:11 +0500

    add game component

---
+++ src/components/Game/index.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from 'vue'
+
+export default { 
+  props: {
+  step: String,
+  question: Object,
+  onClickVariant: Function,
+  questions: Array,
+  }
+}
+
+//const percent = Math.round(step / questions.length * 100);
+
+</script>
+
+<template>
+  <div>
+
+        <h1>{{questions.question}}</h1>
+  </div>
+</template>
+
+<style scoped>
+@import "./style.scss";
+</style>
