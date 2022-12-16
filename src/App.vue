959a0ad04ceedfc728998f69fa444066b914572c
evgenyvodyannikov <vodyannikov.eugene@gmail.com>
2022-12-16 20:40:21 +0500

    fetch data from quizapi

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,41 @@
-<script setup>
-import Game from './components/Game/index.vue';
-import { ref } from "vue";
-
-
-var url = new URL('https://quizapi.io/api/v1/questions');
-var params = [['apiKey', 'F2vPTpqtEbEd9489JM5TeVayeUohhXF5hbnXw2iS'], ['limit', '9']]; // , ['category', 'Linux']
-
-url.search = new URLSearchParams(params).toString();
-
-fetch(url)
-  .then(res => res.json())
-  .then((data) => {
-    console.log(data);
-    questions.value = data;
-    console.log(questions);
-    isFetched.value = true;
-  })
-  .catch("Ошибка получения данных с сервера.\nПерезагрузите страницу.");
-
-const questions = ref([]);
-let isFetched = ref(false);
-
-</script>
-
 <template>
-  <Game :questions=questions />
+  <Game/>
 </template>
+
+<script>
+import Game from './components/Game/index.vue';
+export default {
+  name: 'app',
+  data() {
+    return {
+      posts: [],
+    }
+  },
+  created() {
+    this.fetchData();
+  },
+  components: {
+    Game
+  },
+  methods: {
+    fetchData() {
+
+      const url = new URL('https://quizapi.io/api/v1/questions');
+      const params = [['apiKey', 'F2vPTpqtEbEd9489JM5TeVayeUohhXF5hbnXw2iS'], ['limit', '9']]; // , ['category', 'Linux']
+      
+      url.search = new URLSearchParams(params).toString();
+
+      fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        this.posts = data;
+        console.log(this.posts);
+      });
+    }
+  },
+}
+ 
+</script>
 
 <style scoped>
 
--- src/components/Game/index.vue
+++ src/components/Game/index.vue
@@ -1,25 +1,12 @@
-<script setup>
-import { ref } from 'vue'
-
-export default { 
-  props: {
-  step: String,
-  question: Object,
-  onClickVariant: Function,
-  questions: Array,
-  }
-}
-
-//const percent = Math.round(step / questions.length * 100);
-
-</script>
-
 <template>
   <div>
-
-        <h1>{{questions.question}}</h1>
+     <h1>FFFF</h1>
   </div>
 </template>
+
+<script setup>
+
+</script>
 
 <style scoped>
 @import "./style.scss";
