3c495212e781bba386efafbbc13e355e516285f4
evgenyvodyannikov <vodyannikov.eugene@gmail.com>
2022-12-19 22:23:15 +0500

    fix styles 2x

--- src/components/Result/index.vue
+++ src/components/Result/index.vue
@@ -4,7 +4,7 @@
             <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
             <h2>Ваш результат {{score}} из {{length}}</h2>
             <a href="/">
-                <button class="button">Попробовать снова</button>
+                <button>Попробовать снова</button>
             </a>
         </div>
     </div>
